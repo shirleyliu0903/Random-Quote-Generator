2747e6a2b8192fa231f81e459833a21353407d7b
Shirley Liu <shirleyliu@Shirleys-MacBook-Air.local>
2017-11-10 15:18:33 -0500

    quotes are generating

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,18 @@
  $(document).ready(function() {
- 
 
+  	var fakeQuotes = ["Hi", "Bye", "Why", "Lie" ];
+
+  	function myQuote() {
+  		var num = Math.floor(Math.random() * fakeQuotes.length);
+  		return fakeQuotes[num]; 
+  	};
+  	
+  	console.log(Math.floor(0.4))
+
+ 	$("#getQuote").click(function() {
+ 		$("#quotes").text(myQuote());
+	});
 
     
  });
+
